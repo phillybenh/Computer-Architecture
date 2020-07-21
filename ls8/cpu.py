15fc91a4b1d62555bccdd7cc5ed10a7de4dfbc1b
Ben Haus <ben@little-haus.com>
2020-07-21 15:05:43 -0400

    day-2 initial commit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -32,17 +32,17 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        # # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
         for instruction in program:
             self.ram[address] = instruction
@@ -82,7 +82,6 @@ class CPU:
         self.running = False
 
     def ldi(self, operand_a, operand_b):
-
         self.reg[operand_a] = operand_b
         self.pc += 3
     
